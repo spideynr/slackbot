78f1dc9aaa139ddead47e6fe75cb18bc44c9a543
Nick Rossolatos <spidey_nr@hotmail.com>
2016-03-07 17:34:55 +0200

    athens dev response

--- app/controllers/team_join.js
+++ app/controllers/team_join.js
@@ -8,9 +8,9 @@ module.exports = (message) => {
 
   var generalChannel = dataStore.getChannelByName('general');
   var codingChannel = dataStore.getChannelByName('coding');
-  var skgtechChannel = dataStore.getChannelByName('skgtech_io');
-  var jobsChannel = dataStore.getChannelByName('jobs');
-  var devitChannel = dataStore.getChannelByName('devitconf');
+  var eventsChannel = dataStore.getChannelByName('events');
+  var jobsChannel = dataStore.getChannelByName('job-offers');
+  var showcaseChannel = dataStore.getChannelByName('projects_showcase');
 
   var user = message.user;
 
@@ -18,12 +18,9 @@ module.exports = (message) => {
    * @see https://api.slack.com/docs/formatting
    */
   var response = `<@${user.id}|${user.name}> Hello! ` +
-                 `This is the advanced AI(a level beyond R2D2) bot built by the SKGTech team. I am welcoming you aboard. ` +
-                 `This is the <#${generalChannel.id}|${generalChannel.name}> channel where we usually hang out. There is also the <#${codingChannel.id}|${codingChannel.name}>, the <#${skgtechChannel.id}|${skgtechChannel.name}> to get involved in our community, the <#${jobsChannel.id}|${jobsChannel.name}> to post a job and many more. ` +
-                 `Also check our currently running projects: ` +
-                 `* DEVit Conference 2016: Wanna get involved? Go to <#${devitChannel.id}|${devitChannel.name}> ` +
-                 `* Official SKGTech site needs your help. Give us a hand: https://github.com/skgtech/skgtech.github.io/issues ` +
-                 `* SKG Together event: Got a team in Thessaloniki? Tell us about it.`;
+                 `This is the advanced AI(a level beyond R2D2) bot built by the Athens-Devs team. I am welcoming you aboard. ` +
+                 `This is the <#${generalChannel.id}|${generalChannel.name}> channel where we usually hang out. There is also the <#${codingChannel.id}|${codingChannel.name}>, the <#${events.id}|${events.name}> to get involved in our community and see what events are available, the <#${jobsChannel.id}|${jobsChannel.name}> to post a job and many more. ` +
+                 `Also showcase your projects and get feeback at the <#${showcaseChannel.id}|${showcaseChannel.name}> channel`;
 
   webClient.dm.open(user.id,function(err,data){
     rtmClient.sendMessage(response,data.channel.id,function(){
